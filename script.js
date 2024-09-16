0dfb5aa6638c709095e800c861be46231d9ad6bc
Bhankid <alfredfianyo50@gmail.com>
2024-09-16 19:03:43 +0000

    categories update 1.1

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ document.getElementById("mobile-nav-toggle")
       mobileNavContent.style.display === "block" ? "none" : "block";
     this.classList.toggle("active");
   });
+
+  // setTimeout(function () {
+  //   const buttons = document.querySelectorAll(".cta-tertiary");
+  //   buttons.forEach((button) => {
+  //     button.innerHTML = "Coming soon...";
+  //   });
+// }, 5000);
+  
+let buttons = document.querySelectorAll(".cta-tertiary");
+let originalContent = [];
+buttons.forEach((button) => {
+  originalContent.push(button.innerHTML);
+});
+
+let comingSoon = "Coming Soon...";
+let currentState = 0;
+
+setInterval(function () {
+  currentState = (currentState + 1) % 2;
+  buttons.forEach((button, index) => {
+    if (currentState === 0) {
+      button.innerHTML = originalContent[index];
+    } else {
+      button.innerHTML = comingSoon;
+    }
+  });
+}, 4000);
