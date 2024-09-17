e3a83af34d7b17d1344737f20bdb46ba03c38423
Bhankid <alfredfianyo50@gmail.com>
2024-09-17 05:24:25 +0000

    msg.cat.mob

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
+const mobileNavToggle = document.getElementById("mobile-nav-toggle");
+const navContent = document.querySelector(".nav-content");
 
-document.getElementById("mobile-nav-toggle")
-  .addEventListener("click", function () {
-    var mobileNavContent = document.getElementById("mobile-nav-content");
-    mobileNavContent.style.display =
-      mobileNavContent.style.display === "block" ? "none" : "block";
-    this.classList.toggle("active");
-  });
-
-  // setTimeout(function () {
-  //   const buttons = document.querySelectorAll(".cta-tertiary");
-  //   buttons.forEach((button) => {
-  //     button.innerHTML = "Coming soon...";
-  //   });
-// }, 5000);
-  
-let buttons = document.querySelectorAll(".cta-tertiary");
-let originalContent = [];
-buttons.forEach((button) => {
-  originalContent.push(button.innerHTML);
+mobileNavToggle.addEventListener("click", () => {
+  navContent.classList.toggle("active");
+  mobileNavToggle.classList.toggle("bx-chevron-up");
 });
 
-let comingSoon = "Coming Soon...";
-let currentState = 0;
+// Add event listener to close the menu when clicking outside
+document.addEventListener("click", (e) => {
+  if (!navContent.contains(e.target) && !mobileNavToggle.contains(e.target)) {
+    navContent.classList.remove("active");
+    mobileNavToggle.classList.remove("bx-chevron-up");
+  }
+});
 
-setInterval(function () {
-  currentState = (currentState + 1) % 2;
-  buttons.forEach((button, index) => {
-    if (currentState === 0) {
-      button.innerHTML = originalContent[index];
-    } else {
-      button.innerHTML = comingSoon;
-    }
-  });
-}, 4000);
+// setTimeout(function () {
+//   const buttons = document.querySelectorAll(".cta-tertiary");
+//   buttons.forEach((button) => {
+//     button.innerHTML = "Coming soon...";
+//   });
+// }, 5000);
+
+// let buttons = document.querySelectorAll(".cta-tertiary");
+// let originalContent = [];
+// buttons.forEach((button) => {
+//   originalContent.push(button.innerHTML);
+// });
+
+// let comingSoon = "Coming Soon...";
+// let currentState = 0;
+
+// setInterval(function () {
+//   currentState = (currentState + 1) % 2;
+//   buttons.forEach((button, index) => {
+//     if (currentState === 0) {
+//       button.innerHTML = originalContent[index];
+//     } else {
+//       button.innerHTML = comingSoon;
+//     }
+//   });
+// }, 4000);
